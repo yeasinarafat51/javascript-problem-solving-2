ad0c3f4e8b41c587d6c8af9f061c5e252d060e53
yeasinarafat51 <aloondokar65@gmail.com>
2024-07-28 23:27:45 +0600

    first commit

---
+++ problem2.js
@@ -0,0 +1,32 @@
+ // question 1
+ const person = {
+    name: "John",
+    age: 30,
+    occupation: "Developer"
+  };
+  
+  // Destructuring
+  const { name, age } = person;
+  
+  console.log(name); // "John"
+  console.log(age);  // 30
+  
+
+
+
+
+
+ // question 2
+ 
+
+
+
+
+ // question 1
+ // question 1
+ // question 1
+ // question 1
+ // question 1
+ // question 1
+ // question 1
+ // question 1
